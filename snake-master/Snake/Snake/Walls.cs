08c270df0a869c7d1b6416d41315d2708922b395
Nicolas Vallet <nicolas.vallet@atos.net>
2018-04-23 14:25:06 +0200

    MAJ Walls.cs

--- snake-master/Snake/Snake/Walls.cs
+++ snake-master/Snake/Snake/Walls.cs
@@ -14,7 +14,7 @@ namespace Snake
 		{
 			wallList = new List<Figure>();
 
-			// Отрисовка рамочки
+			// Toto comprend rieng
 			HorizontalLine upLine = new HorizontalLine( 0, mapWidth - 2, 0, '+' );
 			HorizontalLine downLine = new HorizontalLine( 0, mapWidth - 2, mapHeight - 1, '+' );
 			VerticalLine leftLine = new VerticalLine( 0, mapHeight - 1, 0, '+' );
