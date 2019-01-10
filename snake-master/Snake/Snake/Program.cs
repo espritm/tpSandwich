0d8187b7fa928252d5970e72f6b13fc44e2ad3e4
vergerk <33520269+vergerk@users.noreply.github.com>
2019-01-10 14:55:08 +0100

    -changement des caractères de nourriture et du snake

--- snake-master/Snake/Snake/Program.cs
+++ snake-master/Snake/Snake/Program.cs
@@ -11,20 +11,20 @@ namespace Snake
 		static void Main( string[] args )
 		{
             //Console.SetBufferSize( 80, 25 );
-            
+
 			Walls walls = new Walls( 80, 25 );
 			walls.Draw();
 
 			// Отрисовка точек			
-			Point p = new Point( 4, 5, '*' );
+			Point p = new Point( 4, 5, '#' );
 			Snake snake = new Snake( p, 4, Direction.RIGHT );
 			snake.Draw();
 
-			FoodCreator foodCreator = new FoodCreator( 80, 25, '$' );
+			FoodCreator foodCreator = new FoodCreator( 80, 25, '@' );
 			Point food = foodCreator.CreateFood();
 			food.Draw();
 
-			while (true)
+            while (true)
 			{
 				if ( walls.IsHit(snake) || snake.IsHitTail() )
 				{
